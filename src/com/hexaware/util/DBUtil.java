ac5e52454decd8a1d199bb98d615c351c2cefd13
tmkha <tmkha@DESKTOP-GRV739V>
2023-12-18 21:56:07 +0530

    First Commit

---
+++ src/com/hexaware/util/DBUtil.java
@@ -0,0 +1,36 @@
+package com.hexaware.util;
+
+import java.sql.Connection;
+//import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.hexaware.dao.ServiceProvider;
+
+public class DBUtil implements ServiceProvider {
+	private static final String JDBC_URL = "jdbc:mysql://localhost/hexa";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    static {
+        try {
+            // Load the JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load JDBC driver");
+        }
+    }
+    
+    @Override
+    public Connection getConnection() {
+        try {
+            // Create a JDBC connection
+            return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to obtain a JDBC connection");
+        }
+    }
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Hx_1079_Java_CodingChallenge {
+	requires java.sql;
+}
